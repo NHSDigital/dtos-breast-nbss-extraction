b59c0b0af60f5518c316cc155cf7baa946217857
wlyagoba <william.lyagoba1@nhs.net>
2026-05-08 14:35:51 +0100

    DSTA-536: refactor pipeline to  use .py rather .ipynb

--- scripts/githooks/check-file-format.sh
+++ scripts/githooks/check-file-format.sh
@@ -81,9 +81,7 @@ function run-editorconfig-natively() {
 
   # shellcheck disable=SC2046,SC2086
   ec \
-    --exclude '.git/' \
-    --exclude '*.ipynb' \
-    $dry_run_opt $($filter)
+    --exclude '.git/' $dry_run_opt $($filter)
 }
 
 # Run editorconfig in a Docker container.
@@ -103,7 +101,7 @@ function run-editorconfig-in-docker() {
   docker run --rm --platform linux/amd64 \
     --volume "$PWD":/check \
     "$image" \
-      sh -c "ec --exclude '.git/' --exclude '*.ipynb' $dry_run_opt \$($filter) /dev/null"
+      sh -c "ec --exclude '.git/' $dry_run_opt \$($filter) /dev/null"
 }
 
 # ==============================================================================
