08698068ae019dc2599bf25b1da2f65f45d7c9ff
wlyagoba <william.lyagoba1@nhs.net>
2026-05-28 09:24:16 +0100

    DSTA-560: Updated to pull all base tables

--- nbss/playcd/export_app_tables.py
+++ nbss/playcd/export_app_tables.py
@@ -2,7 +2,7 @@
 Description:
 Connects to InterSystems Cache via ODBC (DSN=NBSS_64), fetches the first 5 tables from the APP schema,
 loads each into a pandas DataFrame and exports to CSV.
-
+Note: retireved empty tables, in additon to the tables with data to the structure
 """
 import pyodbc
 import pandas as pd
@@ -13,10 +13,9 @@ import sys
 
 # variables
 load_dotenv()
-DSN        = os.getenv("DSN")
-UID        = os.getenv("UID")
-PWD        = os.getenv("PWD")
-SCHEMA     = "APP" # feel free to change this to target a different schema
+DSN = os.getenv("DSN")
+UID = os.getenv("UID")
+PWD = os.getenv("PWD")
 OUTPUT_DIR = "cache_data_export"
 CHUNK_SIZE = 10000
 
@@ -35,50 +34,36 @@ def main():
     # get first 5 tables from APP schema
     # feel to change query to pick up more tables or filter differently
     cursor.execute(f"""
-        SELECT TOP 5 TABLE_SCHEMA, TABLE_NAME
+        SELECT TABLE_SCHEMA, TABLE_NAME
         FROM INFORMATION_SCHEMA.TABLES
-        WHERE TABLE_SCHEMA = '{SCHEMA}'
-        ORDER BY TABLE_SCHEMA, TABLE_NAME DESC
+        WHERE TABLE_TYPE ='BASE TABLE'
     """)
 
     tables = cursor.fetchall()
-
-    print(f"📋 Tables found in [{SCHEMA}] schema:")
-    for schema, table in tables:
-        print(f"   {schema}.{table}")
-
     os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-    # loop through tables, fetch data, export to .csv
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
     print("\nFetching data from tables\n")
 
     for schema, table in tables:
         try:
             full_table_name = f"{schema}.{table}"
-            SQL = f"SELECT * FROM {full_table_name}"
+            cursor.execute(f"SELECT * FROM {full_table_name}")
+            columns = [col[0] for col in cursor.description]
 
-            cursor.execute(SQL)
-            columns   = [col[0] for col in cursor.description]
-            chunks    = []
-            total_rows = 0
-
-            while True:
-                rows = cursor.fetchmany(CHUNK_SIZE)
-                if not rows:
-                    break
+            chunks = []
+            while rows := cursor.fetchmany(CHUNK_SIZE):
                 chunks.append(pd.DataFrame.from_records(rows, columns=columns))
-                total_rows += len(rows)
 
             df = pd.concat(chunks, ignore_index=True) if chunks else pd.DataFrame(columns=columns)
 
             # save files
-            filename    = f"{schema}_{table}_{timestamp}.csv"
-            output_path = os.path.join(OUTPUT_DIR, filename)
+            filename    = f"{table}.csv"
+            schema_dir  = f"{OUTPUT_DIR}/{schema}"
+            os.makedirs(schema_dir, exist_ok=True)
+            output_path = os.path.join(schema_dir, f"{table}.csv")
             df.to_csv(output_path, index=False)
 
-            print(f"{full_table_name:<10} - {df.shape[0]:>6,} rows x {df.shape[1]} cols → {filename}")
+            print(f"{full_table_name:<10} - {df.shape[0]:>6,} rows x {df.shape[1]} cols → {table}.csv")
 
         except Exception as e:
             print(f"{schema}.{table} failed: {e}")
@@ -88,7 +73,6 @@ def main():
     conn.close()
 
     print(f"\nAll CSVs saved to: {os.path.abspath(OUTPUT_DIR)}")
-
 
 if __name__ == "__main__":
     main()
