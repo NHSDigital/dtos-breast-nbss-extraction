30d58ccaf7f9c0e30aa49de2e6ace4c3e0d875b5
Will Lyagoba <william.lyagoba1@nhs.net>
2026-07-06 16:37:29 +0100

    DSTA-568: add .md file for creating admin user (#41)
    * DSTA-568: add .md file for creating admin user
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: add ps file to create new user
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: updated changes suggested by co-pilot
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: updated markdown file
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Front and Back end user created
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Updated readme
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Description for script
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Update formatting
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Update formatting
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: added section for  resetting password
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Changes from reviews  corrections
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Changes to  option 2 script
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-570: Updated mardown for username
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    * DSTA-568: Review Revises
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>
    
    ---------
    
    Signed-off-by: wlyagoba <william.lyagoba1@nhs.net>

---
+++ nbss/playcd/new_cache_user.ps1
@@ -0,0 +1,58 @@
+<#
+DESCRIPTION
+Creates a new user in both the Caché backend (%SYS) and the NBSS frontend application (NBSS_DEM).
+    Step 1 — Registers the user as a Caché system user in %SYS with the roles needed  to connect to the NBSS_DEM database.
+    Step 2 — Creates the corresponding NBSS application user in the UTIL.Users table so the user can log in to the NBSS frontend. On first login, NBSS will prompt the user to set a new password.
+#>
+
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)][string]$NewUsername,
+    [Parameter(Mandatory)][string]$NewPassword,
+    [string]$CacheInstance = "CACHE",
+    [string]$AdminUser     = "_SYSTEM",
+    [Parameter(Mandatory)][string]$AdminPassword,
+    [string]$CsessionPath  = "C:\InterSystems\Cache\bin\csession.exe"
+)
+
+# Step 1
+# csession does not support -P for password.
+# We pipe the username and password as interactive input, followed by the COS commands.
+$sysScript = @"
+$AdminUser
+$AdminPassword
+ZN ""%SYS""
+SET sc = ##class(Security.Users).Create("$NewUsername","%SYS","$NewPassword")
+WRITE `$SYSTEM.Status.GetErrorText(sc),!
+SET sc = ##class(Security.Users).AddRoles("$NewUsername","%All")
+DO ##class(Security.Users).Get("$NewUsername",.props)
+WRITE "Roles: ",props("Roles"),!
+WRITE "Enabled: ",props("Enabled"),!
+HALT
+"@
+
+Write-Host "--- Creating Cache system user in %SYS ---"
+$sysScript | & $CsessionPath $CacheInstance -U "%SYS"
+
+
+# Step 2
+$nbssScript = @"
+$AdminUser
+$AdminPassword
+ZN "NBSS_DEM"
+SET obj = ##class(UTIL.Users).%New()
+SET obj.UserId = "$NewUsername"
+SET obj.UserForename = "NBSS"
+SET obj.UserSurname = "Extraction"
+SET obj.UserGroupId = "SOM"
+SET obj.UserSystemManager = 1
+SET obj.UserDisabled = 0
+SET obj.UserForcePasswordChange = 1
+SET obj.ResetPassword = 1
+SET sc = obj.%Save()
+WRITE `$SYSTEM.Status.GetErrorText(sc),!
+HALT
+"@
+
+Write-Host "--- Creating NBSS application user in NBSS_DEM ---"
+$nbssScript | & $CsessionPath $CacheInstance -U "NBSS_DEM"
