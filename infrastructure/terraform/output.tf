f88586ed2ac7c99e391813ccf0ceb90e54c9d39c
Michael Justus <209924279+micjustus-nc@users.noreply.github.com>
2026-09-16 15:28:21 +0000

    DSTA-599: Secure BSO container creation (#74)
    * DSTA-599: add support to upload one or more containers for upload.
    
    * DSTA-599: add storage upload tests
    
    Tests allow users to upload using SAS tokens and Entra ID authentication.
    Add support to specify a command line argument to generate user-delgation SAS tokens with -GenerateSasToken.
    Ensure both tests (azstorage and azcopy) follow the same processing pattern for consistency.
    Ensure PS script uses $LASTEXITCODE where critical checks are required to ensure calls don't proceed in a potentially failed state.

--- infrastructure/terraform/output.tf
+++ infrastructure/terraform/output.tf
@@ -1,3 +1,6 @@
-output "upload_container_url" {
-  value = "${azurerm_storage_account.upload.primary_blob_endpoint}${azurerm_storage_container.upload.name}"
+output "upload_container_urls" {
+  value = {
+    for container_key, container in azurerm_storage_container.bso :
+    container_key => "${azurerm_storage_account.upload.primary_blob_endpoint}${container.name}"
+  }
 }
