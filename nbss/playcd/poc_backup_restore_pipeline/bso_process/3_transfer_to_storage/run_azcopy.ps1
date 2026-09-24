bf3ad70104bfd94acf277753ae2e162ea0ae7417
samw-nhs <172405776+samw-nhs@users.noreply.github.com>
2026-09-24 13:46:27 +0100

    DSTA-690: add script to run azcopy

---
+++ nbss/playcd/poc_backup_restore_pipeline/bso_process/3_transfer_to_storage/run_azcopy.ps1
@@ -0,0 +1,36 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$StorageAccountName = "sanbssedevupload",
+    [string]$ContainerName = "uploads",
+    [string]$LocalFilePath = "",
+    [string]$TenantId = ""
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+
+$containerUrl = "https://$StorageAccountName.blob.core.windows.net/$ContainerName"
+
+Write-Host "Parameters" -ForegroundColor Yellow
+Write-Host "   Container endpoint: " -NoNewline
+Write-Host "$containerUrl" -ForegroundColor DarkYellow
+Write-Host "   File to upload: " -NoNewline
+Write-Host "$LocalFilePath" -ForegroundColor DarkYellow
+
+Write-Host "Uploading with AzCopy (auth=Entra)..." -ForegroundColor Yellow
+
+azcopy login --tenant-id $TenantId
+azcopy copy "$LocalFilePath" "$containerUrl" `
+    --from-to=LocalBlob `
+    --overwrite=true ` # do we want this flag or not?
+    --blob-type=BlockBlob `
+    --log-level=INFO `
+    --put-md5
+
+if ($LASTEXITCODE -ne 0) {
+    throw "❌ AzCopy upload with Entra authentication failed. Exit code = $LASTEXITCODE"
+}
+
+Write-Host "   ✅ Successfully uploaded."
+Write-Host ""
